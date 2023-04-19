7cc38eabae2cb946e942a7ea09ab29c610b7f893
Kevin Cefalu <kevin.cefalu@gmail.com>
2023-04-19 15:11:13 -0500

    repo cleanup

--- src/main.py
+++ src/main.py
@@ -1,42 +1,28 @@
 import time
 import numpy as np
+import serial
 from gpiozero import Button, RGBLED
 from colorzero import Color
-import serial
 import adafruit_thermal_printer
 
 # Define the pins
-
 btnPin = 23
 btnLedRedPin = 12
 btnLedGreenPin = 13
 btnLedBluePin = 19
 
+# Define the printer settings
+serialPath = "/dev/serial0"
+printerClass = 2.69
 printerBaudRate = 9600
-printerTimeout = 5
+printerTimeout = 3000
 
 btn = Button(btnPin)
 btnLed = RGBLED(btnLedRedPin, btnLedGreenPin, btnLedBluePin, False)
 
-ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.69)
-uart = serial.Serial("/dev/serial0", baudrate=9600, timeout=3000)
-printer = ThermalPrinter(uart)
-
-def linear(steps):
-  for t in range(steps):
-    yield t / (steps - 1)
-
-def ease_in(steps):
-  for t in linear(steps):
-    yield t ** 2
-
-def ease_out(steps):
-  for t in linear(steps):
-    yield t * (2 - t)
-
-def ease_in_out(steps):
-  for t in linear(steps):
-    yield 2 * t * t if t < 0.5 else (4 - 2 * t) * t - 1
+thermalPrinterClass = adafruit_thermal_printer.get_printer_class(printerClass)
+uart = serial.Serial(serialPath, baudrate = printerBaudRate, timeout = printerTimeout)
+printer = thermalPrinterClass(uart)
 
 def printWorkDetails():
   printer.warm_up()
@@ -52,20 +38,17 @@ def printWorkDetails():
   printer.print("kcefalu@netchexonline.com")
   printer.feed(4)
 
+def fadeFromToColor(fromColor, toColor, steps = 100, delay = 0.025):
+  for color in fromColor.gradient(toColor, steps = steps):
+    btnLed.color = color
+    time.sleep(delay)
+
 def onButtonHeld(btn):
-  for color in Color('black').gradient(Color('red'), steps = 100):
-    btnLed.color = color
-    time.sleep(0.025)
-
+  fadeFromToColor(Color('black'), Color('yellow'))
   time.sleep(2)
-
   printWorkDetails()
-
-  for color in Color('red').gradient(Color('green'), steps = 100):
-    btnLed.color = color
-    time.sleep(0.025)
-
-  time.sleep(5)
+  fadeFromToColor(Color('yellow'), Color('green'))
+  time.sleep(2)
   btnLed.color = Color('black')
 
 btn.when_held = onButtonHeld
