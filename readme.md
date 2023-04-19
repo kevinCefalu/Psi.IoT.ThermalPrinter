223608f082902eed3e152d06a2e4e4fc8af8d81c
Kevin Cefalu <kevin.cefalu@gmail.com>
2023-04-19 13:21:16 -0500

    new version?

--- readme.md
+++ readme.md
@@ -21,3 +21,7 @@ This project is the software for a portable thermal printer project. Initially i
 - [Rugged Metal Pushbutton - 16mm 6V RGB Momentary](https://www.adafruit.com/product/3350)
 - [Mini Thermal Receipt Printers](https://learn.adafruit.com/mini-thermal-receipt-printer)
 - [Raspberry Pi Zero 2 W](https://www.raspberrypi.com/products/raspberry-pi-zero-2-w/)
+- [Raspberry Pi PinOut](https://pinout.xyz/pinout/)
+- [Raspberry Pi GPIO PWM](https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/)
+- [Generate 24-bit color from RGB LED with Raspberry Pi](https://medium.com/@palmbook/generate-24-bit-color-from-rgb-led-with-raspberry-pi-342b1fb92dcc)
+- [RPI Zero 2W Board Layout: GPIO Pinout, Specs, Schematic in detail](https://www.etechnophiles.com/rpi-zero-2w-board-layout-pinout-specs-price/#rpi-zero-2w-gpio-pinout-in-detail)
---
+++ src/main.v2.py
@@ -0,0 +1,68 @@
+import time
+import numpy as np
+from gpiozero import Button, RGBLED
+from colorzero import Color
+
+# Define the pins
+
+btnPin = 23
+btnLedRedPin = 12
+btnLedGreenPin = 13
+btnLedBluePin = 19
+# btnBounceTime = 400
+
+printerBaudRate = 9600
+printerTimeout = 5
+
+btn = Button(btnPin)
+btnLed = RGBLED(btnLedRedPin, btnLedGreenPin, btnLedBluePin, False)
+
+def onButtonHeld(btn):
+  print('Button {btnName} held'.format(btnName = btn.pin.number))
+  # Set LED to RED
+  btnLed.color = Color('red')
+  print('Red')
+  time.sleep(3)
+
+  # Set LED to GREEN
+  btnLed.color = Color('green')
+  print('Green')
+  time.sleep(3)
+
+  # Set LED to BLUE
+  btnLed.color = Color('blue')
+  print('Blue')
+  time.sleep(3)
+
+btn.when_held = onButtonHeld
+
+# running = True
+
+# while running:
+#   try:
+#     # for x in range(100, 0, -1):
+#     #   btnLedRed.ChangeDutyCycle(x)
+#     #   time.sleep(0.025)
+
+#     # Set LED to RED
+#     btnLed.color = Color('red')
+#     print('Red')
+#     time.sleep(3)
+
+#     # Set LED to GREEN
+#     btnLed.color = Color('green')
+#     print('Green')
+#     time.sleep(3)
+
+#     # Set LED to BLUE
+#     btnLed.color = Color('blue')
+#     print('Blue')
+#     time.sleep(3)
+
+#   # On Keyboard Interrupt, stop the loop
+#   except KeyboardInterrupt:
+#     running = False
+
+# Stop PWM
+btn.stop()
+btnLed.close()
